8dc4e6d7b6b67fd569a443f855a2c6b40db865c8
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-24 21:51:47 +0500

    Playbar added at the bottom, JavaScript Updated

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("Hey!");
+
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:3000/Songs/");
+    let response = await a.text();
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+
+    return songs;
+}
+
+async function main() {
+    let songs = await getSongs();
+    console.log(songs);
+
+    var audio = new Audio(songs[0]);
+
+    let playBtn = document.querySelector(".play-button");
+    playBtn.addEventListener(("click"), () => {
+
+        if (audio.paused) {
+            audio.play()
+            playBtn.src = "Resources/pause.svg"
+        }
+        else {
+            playBtn.src = "Resources/song-play.svg"
+            audio.pause()
+        }
+    })
+}
+
+main()
